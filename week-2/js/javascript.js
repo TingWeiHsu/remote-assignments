1325c0361ca71547c42402f5a3bcea33ce69a4be
TingWei Hsu <ck8189@gapp.nthu.edu.tw>
2018-07-08 06:38:18 +0800

    W2 2Version
    revised Call to Action button

--- week-2/js/javascript.js
+++ week-2/js/javascript.js
@@ -7,9 +7,9 @@ document.getElementById("click").innerHTML="<h1>Hava a good time!</h1>";
 
 function myHide() {
     var x = document.getElementById("hidden");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
         x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
 }
